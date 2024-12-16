b99408c83796448d7228398eb4c75c21a67041e9
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-12-16 12:35:45 +0700

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
