446efe133f57be4f23b4566dbc0551603c7d41c1
ajikurniawan076 <ajikurniawan076@gmail.com>
2025-02-05 13:30:29 +0700

    aji

--- backend/controller/laporanPos/laporanPenjualan.js
+++ backend/controller/laporanPos/laporanPenjualan.js
@@ -205,7 +205,7 @@ const laporanPenjualan = asyncHandler(async(req,res)=>{
     }
     const totalTransaksi = transaksi.length;
     
-    res.status(200).json({totalPendapatan : total, totalTransaksi: totalTransaksi})
+    res.status(200).json({transaksi: transaksi, totalPendapatan : total, totalTransaksi: totalTransaksi})
 }
 catch(error){
     res.status(400).json({ message: error.message });
