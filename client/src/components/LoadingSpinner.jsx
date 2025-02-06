5a56eca1758ba8ddd6dd79ac1a3c6b1da92a1f13
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2025-02-06 12:24:06 +0700

    galeri

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LoadingSpinner() {
   return (
     <>
-      <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-full h-[80vh] flex items-center justify-center">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-secondary shadow-primary drop-shadow-sm"></div>
       </div>
     </>
