717e1ad837539d16c8af15730237e02b6e136a3d
OivickoEkaganiIrwanto <oivicko020405@gmail.com>
2025-02-11 23:00:35 +0700

    buka buat super admin

--- backend/routes/admin/adminRoutes.js
+++ backend/routes/admin/adminRoutes.js
@@ -9,7 +9,7 @@ import { protect } from "../../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/registerAdmin",protect([1]), registerAdmin); 
+router.post("/registerAdmin", registerAdmin); 
 router.get("/getAdmins", protect([1]), getAdmins); 
 router.delete("/deleteAdmin/:id", protect([1]), deleteAdmin); 
 router.post("/Login", cekLogin); 
