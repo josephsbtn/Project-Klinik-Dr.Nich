061b3fea6e7385ca79e0867774ab8535df056005
OivickoEkaganiIrwanto <oivicko020405@gmail.com>
2025-02-12 21:30:03 +0700

    Update produkRoutes.js
    protect untuk produk kec untuk get

--- backend/routes/admin/produkRoutes.js
+++ backend/routes/admin/produkRoutes.js
@@ -34,34 +34,34 @@ const router = express.Router();
 
 //CAROUSEL PRODUCT
 router.post("/newImage",protect([1,4]), newImage);
-router.get("/getImage",protect([1,4]), getImage);
+router.get("/getImage", getImage);
 router.put("/updateImage/:id",protect([1,4]), updateImage);
 router.delete("/deleteImage/:id",protect([1,4]), deleteImage);
 
 //PRODUCT
 router.post("/tambahproduk",protect([1,4]), newproduk);
-router.get("/getAllProduk",protect([1,4]), getproduk);
+router.get("/getAllProduk", getproduk);
 router.put("/updateproduk/:id",protect([1,4]), updateproduk);
 router.delete("/deleteproduk/:id",protect([1,4]), deleteproduk);
-router.get("/getprodukbyId/:id",protect([1,4]), getprodukbyID);
-router.get("/getProductByCategory/:id",protect([1,4]), getprodukbycategory);
+router.get("/getprodukbyId/:id", getprodukbyID);
+router.get("/getProductByCategory/:id", getprodukbycategory);
 
 //PRODUCT CATEGORY
 router.post("/tambahkategoriProduk",protect([1,4]), newCategoryProduct);
-router.get("/getAllkategoriProduk",protect([1,4]), getCategoryProduct);
+router.get("/getAllkategoriProduk", getCategoryProduct);
 router.delete("/deletekategoriProduk/:id",protect([1,4]), deleteCategoryProduct);
 router.put("/editkategoriProduk/:id",protect([1,4]), editCategoryProduct);
-router.get("/getCategoryById/:id",protect([1,4]), getCategoryById);
+router.get("/getCategoryById/:id", getCategoryById);
 
 //PRODUCT TYPE
 router.post("/tambahproductType",protect([1,4]), newProductType);
-router.get("/getAllproductType",protect([1,4]), getProductType);
+router.get("/getAllproductType", getProductType);
 router.put("/editproductType/:id",protect([1,4]), editProductType);
 router.delete("/deleteproductType/:id",protect([1,4]), deleteProductType);
 
 //TIPE KULIT
 router.post("/createtipeKulit",protect([1,4]), newtipeKulit);
-router.get("/getAlltipeKulit",protect([1,4]), gettipeKulit);
+router.get("/getAlltipeKulit", gettipeKulit);
 router.put("/edittipeKulit/:id",protect([1,4]), updatetipeKulit);
 router.delete("/deletetipeKulit/:id",protect([1,4]), deletetipeKulit);
 
