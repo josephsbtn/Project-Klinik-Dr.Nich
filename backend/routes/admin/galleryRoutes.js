b676bd1dd5a321112f69986d30f5dfe796e5f1c2
OivickoEkaganiIrwanto <oivicko020405@gmail.com>
2025-02-12 13:47:57 +0700

    Update galleryRoutes.js

--- backend/routes/admin/galleryRoutes.js
+++ backend/routes/admin/galleryRoutes.js
@@ -10,7 +10,7 @@ import { protect } from "../../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/createGaleri",(protect([1]) || protect([4])), newGaleri);
-router.get("/getAllGaleri",(protect([1]) || protect([4])), getGaleri);
+router.get("/getAllGaleri",protect([1,4]), getGaleri);
 router.get("/getGaleriById/:id",(protect([1]) || protect([4])), getGaleriById);
 router.put("/editGaleri/:id",(protect([1]) || protect([4])), editGaleri);
 router.delete("/deleteGaleri/:id",(protect([1]) || protect([4])), deleteGaleri);
