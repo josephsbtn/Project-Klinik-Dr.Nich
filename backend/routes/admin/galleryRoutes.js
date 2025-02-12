633bb99ae762cd6dcaba5ff55b77b9015cfdf5d0
OivickoEkaganiIrwanto <oivicko020405@gmail.com>
2025-02-12 13:52:49 +0700

    protect gallery
    allow level 1 & 4

--- backend/routes/admin/galleryRoutes.js
+++ backend/routes/admin/galleryRoutes.js
@@ -9,10 +9,10 @@ import {
 import { protect } from "../../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.post("/createGaleri",(protect([1]) || protect([4])), newGaleri);
+router.post("/createGaleri",protect([1,4]), newGaleri);
 router.get("/getAllGaleri",protect([1,4]), getGaleri);
-router.get("/getGaleriById/:id",(protect([1]) || protect([4])), getGaleriById);
-router.put("/editGaleri/:id",(protect([1]) || protect([4])), editGaleri);
-router.delete("/deleteGaleri/:id",(protect([1]) || protect([4])), deleteGaleri);
+router.get("/getGaleriById/:id",protect([1,4]), getGaleriById);
+router.put("/editGaleri/:id",protect([1,4]), editGaleri);
+router.delete("/deleteGaleri/:id",protect([1,4]), deleteGaleri);
 
 export default router;
