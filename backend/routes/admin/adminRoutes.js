5e77a57af586f0a25c2f9a684a57eec373355571
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2025-02-13 18:24:37 +0700

    Update adminRoutes.js

--- backend/routes/admin/adminRoutes.js
+++ backend/routes/admin/adminRoutes.js
@@ -9,7 +9,7 @@ import { protect } from "../../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/registerAdmin", registerAdmin);
+router.post("/registerAdmin", protect([1]), registerAdmin);
 router.get("/getAdmins", protect([1]), getAdmins);
 router.delete("/deleteAdmin/:id", protect([1]), deleteAdmin);
 router.post("/Login", cekLogin);
