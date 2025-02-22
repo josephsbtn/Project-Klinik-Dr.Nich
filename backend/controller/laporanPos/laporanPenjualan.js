9acece51522c2f2bd27885158f058945b94a013b
ajikurniawan076 <ajikurniawan076@gmail.com>
2025-02-22 09:49:53 +0700

    aji

--- backend/controller/laporanPos/laporanPenjualan.js
+++ backend/controller/laporanPos/laporanPenjualan.js
@@ -196,7 +196,6 @@ const laporanTerlaris = asyncHandler(async(req,res)=>{
           kategori : citem.produk.kategori.kategori,
           jumlah : citem.jumlah,
           pendapatan : (citem.jumlah*citem.produk.hargaJual),
-          jenis : citem.jenis
         }
         kategorilist.push(isi)
       }
@@ -206,7 +205,6 @@ const laporanTerlaris = asyncHandler(async(req,res)=>{
           namaProduk : citem.produk.namaProduk,
           jumlah : citem.jumlah,
           pendapatan : (citem.jumlah*citem.produk.hargaJual),
-          jenis : citem.jenis
         }
         produklist.push(isi)
         if(kategorilist.some(item => item.kategori == citem.produk.kategori.kategori)){
@@ -217,7 +215,6 @@ const laporanTerlaris = asyncHandler(async(req,res)=>{
             kategori : citem.produk.kategori.kategori,
             jumlah : citem.jumlah,
             pendapatan : (citem.jumlah*citem.produk.hargaJual),
-            jenis : citem.jenis
           }
           kategorilist.push(isi)
         }
